ffdadd024efcf91650c972697b07199e7b4d16e0
Anas8e <94750646+Anas8e@users.noreply.github.com>
2024-04-02 00:05:48 +0000

    Update 3-hypermedia_del_pagination.py 6

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -69,34 +69,3 @@ class Server:
             'data': page_data,
         }
         return page_info
-
-
-if __name__ == "__main__":
-    server = Server()
-
-    try:
-        server.get_hyper_index(300000, 100)
-    except AssertionError:
-        print("AssertionError raised when out of range")        
-
-    index = 3
-    page_size = 2
-
-    print("Nb items: {}".format(len(server.dataset())))
-
-    # 1- request first index
-    res = server.get_hyper_index(index, page_size)
-    print(res)
-
-    # 2- request next index
-    print(server.get_hyper_index(res.get('next_index'), page_size))
-
-    # 3- remove the first index
-    del server.indexed_dataset()[res.get('index')]
-    print("Nb items: {}".format(len(server.indexed_dataset())))
-
-    # 4- request again the initial index -> the first data retrieves is not the same as the first request
-    print(server.get_hyper_index(index, page_size))
-
-    # 5- request again initial next index -> same data page as the request 2-
-    print(server.get_hyper_index(res.get('next_index'), page_size))
